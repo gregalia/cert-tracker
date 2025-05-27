4edb110777b9f62476db128726bde47203e8b707
Greg Sheppard <gregalia@users.noreply.github.com>
2025-05-28 00:20:48 +0300

    WIP feat: Get certificates for IP addresses
    Currently just logging cert info but will persist certs and scans.

--- main.go
+++ main.go
@@ -2,35 +2,165 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"log/slog"
 	"net"
 	"os"
+	"reflect"
+	"runtime"
+	"strings"
 	"time"
 )
 
 func main() {
-	hostnames := getHostnames()
-	DNSServer := getDNSServer()
-	logger.Info("config read success", "dnsServer", DNSServer, "hostnames", hostnames)
-	resolver := getResolver(DNSServer)
-	logger.Info("got DNS resolver")
-	ipAddresses, err := resolveHostnames(hostnames, resolver)
-	if err != nil{
-		logger.Error("cannot resolve IP Addresses")
+	hostnames := hostnames()
+	DNSServer := dnsServer()
+	logger.Info("configuration", "dnsServer", DNSServer, "hostnames", hostnames)
+	netResolver := resolver(DNSServer)
+	logger.Info("DNS resolver")
+	nameAddressMappings, err := resolve(hostnames, netResolver)
+	if err != nil {
+		logger.Error("cannot resolve IP Addresses", "error", err)
+		os.Exit(1)
 	}
-	logger.Info("got IP addresses", "addresses", ipAddresses)
+	if len(nameAddressMappings) == 0 {
+		logger.Error("cannot resolve IP Addresses")
+		os.Exit(1)
+	}
+	logger.Info("got IP addresses", "addresses", nameAddressMappings)
+	for _, mapping := range nameAddressMappings {
+		for _, ipAddress := range mapping.IPAddresses {
+			certificates(mapping.Hostname, ipAddress)
+		}
+	}
 }
+
+const timeout = time.Second * 30
+const logLocationAt = slog.LevelError // add file + line + function to logs
 
 // TODO: Use net URL hostname?
 type hostname string
 
-var logger = slog.New(slog.NewJSONHandler(os.Stderr, nil))
-
 type config struct {
 	Resolver  string     `json:"resolver"`
 	Hostnames []hostname `json:"hostnames"`
+}
+
+type nameAddressMap struct {
+	Hostname    hostname
+	IPAddresses []net.IP
+}
+
+type hexHandler struct {
+	slog.Handler
+}
+type locationHandler struct {
+	handler slog.Handler
+}
+
+func (h *locationHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.handler.Enabled(ctx, level)
+}
+
+func (h *locationHandler) Handle(ctx context.Context, r slog.Record) error {
+	if r.Level >= logLocationAt {
+		// Skip 3 frames: Handle + log function + user code
+		pc, file, line, ok := runtime.Caller(3)
+		if ok {
+			r.Add(
+				"file", slog.StringValue(file),
+				"line", slog.IntValue(line),
+				"function", slog.StringValue(runtime.FuncForPC(pc).Name()),
+			)
+		}
+	}
+	return h.handler.Handle(ctx, r)
+}
+
+func (h *locationHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &locationHandler{h.handler.WithAttrs(attrs)}
+}
+
+func (h *locationHandler) WithGroup(name string) slog.Handler {
+	return &locationHandler{h.handler.WithGroup(name)}
+}
+
+func (h *hexHandler) Handle(ctx context.Context, r slog.Record) error {
+	// Clone the record so we can modify it
+	r2 := r.Clone()
+
+	// Replace binary values with hex strings
+	r2.Attrs(func(a slog.Attr) bool {
+		if v := a.Value; v.Kind() == slog.KindAny {
+			if b, ok := v.Any().([]byte); ok {
+				r2.Add(a.Key, slog.StringValue(hex.EncodeToString(b)))
+				return false
+			}
+		}
+		return true
+	})
+
+	return h.Handler.Handle(ctx, r2)
+}
+
+var logger = slog.New(&locationHandler{
+	handler: &hexHandler{
+		Handler: slog.NewJSONHandler(os.Stdout, nil),
+	},
+})
+
+func certificates(hostname hostname, ipAddress net.IP) {
+	dialer := &net.Dialer{Timeout: timeout}
+	conn, err := tls.DialWithDialer(
+		dialer,
+		"tcp",
+		net.JoinHostPort(ipAddress.String(), "443"),
+		&tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         string(hostname),
+		})
+	if err != nil {
+		logger.Error("connection error", "error", err)
+		return
+	}
+	defer conn.Close()
+	state := conn.ConnectionState()
+	if len(state.PeerCertificates) == 0 {
+		logger.Warn("no certificates", "hostname", hostname, "ipAddress", ipAddress)
+		return
+	}
+	for i, cert := range state.PeerCertificates {
+		logger.Info("cert info", "hostname", hostname, "ipAddress", ipAddress)
+		logCertDetails(cert, i)
+	}
+}
+
+func logCertDetails(cert *x509.Certificate, index int) {
+	logger.Info("cert info", "index", index)
+
+	v := reflect.ValueOf(*cert)
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		value := v.Field(i)
+
+		// Skip unexported fields
+		// if !field.IsExported() {
+		// 	continue
+		// }
+		if strings.HasPrefix(field.Name, "Raw") {
+			continue
+		}
+
+		logger.Info("cert info", field.Name, value.Interface())
+	}
+	sha256Hash := sha256.Sum256(cert.Raw)
+	sha256HashString := hex.EncodeToString(sha256Hash[:])
+	logger.Info("cert info", "sha256Fingerprint", sha256HashString)
 }
 
 // TODO: Take parameter
@@ -47,7 +177,7 @@ func parseConfig() (*config, error) {
 	return &config, nil
 }
 
-func getHostnames() []hostname {
+func hostnames() []hostname {
 	config, err := parseConfig()
 	if err != nil {
 		logger.Error("cannot get hostnames", "error", err)
@@ -56,7 +186,7 @@ func getHostnames() []hostname {
 	return config.Hostnames
 }
 
-func getDNSServer() net.IP {
+func dnsServer() net.IP {
 	config, err := parseConfig()
 	if err != nil {
 		logger.Error("cannot get resolver", "error", err)
@@ -70,31 +200,43 @@ func getDNSServer() net.IP {
 	return resolver
 }
 
-func getResolver(DNSServer net.IP) *net.Resolver {
+func resolver(dnsServer net.IP) *net.Resolver {
 	return &net.Resolver{
 		PreferGo: true,
 		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
 			dialer := net.Dialer{
-				Timeout: time.Second * 5,
+				Timeout: timeout,
 			}
-			return dialer.DialContext(ctx, network, fmt.Sprintf("%v:53", DNSServer))
+			return dialer.DialContext(
+				ctx,
+				network,
+				net.JoinHostPort(dnsServer.String(), "53"),
+			)
 		},
 	}
 }
 
-func resolveHostnames(hostnames []hostname, resolver *net.Resolver) ([]net.IP, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+func resolve(hostnames []hostname, resolver *net.Resolver) ([]nameAddressMap, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	var results []net.IP
+	var results []nameAddressMap
 	for _, hostname := range hostnames {
-		addresses, err := resolver.LookupIPAddr(ctx, string(hostname))
+		ipAddrs, err := resolver.LookupIPAddr(ctx, string(hostname))
 		if err != nil {
 			return nil, err
 		}
-		for _, address := range addresses {
-			results = append(results, address.IP)
+		var addresses []net.IP
+		for _, address := range ipAddrs {
+			addresses = append(addresses, address.IP)
 		}
+		results = append(
+			results,
+			nameAddressMap{
+				Hostname:    hostname,
+				IPAddresses: addresses,
+			},
+		)
 	}
 	return results, nil
 }
