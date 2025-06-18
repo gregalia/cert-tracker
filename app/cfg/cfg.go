5c434a4a6b5d61cf13bd1d16ee1be125fce5c259
Greg Sheppard <gregalia@users.noreply.github.com>
2025-06-18 19:22:25 +0300

    feat: Use new config.json params and init values

--- app/cfg/cfg.go
+++ app/cfg/cfg.go
@@ -1,28 +1,89 @@
-package config
+package cfg
 
 import (
 	"encoding/json"
+	"log/slog"
+	"net"
 	"os"
+	"time"
+
+	"github.com/go-playground/validator/v10"
 )
 
-// TODO: Use net URL hostname?
-type Hostname string
+const configFilePath = "config.json"
 
-type config struct {
-	Resolver  string     `json:"resolver"`
-	Hostnames []Hostname `json:"hostnames"`
+var Current Params
+var DNSresolvers []net.IP
+var Hostnames []Hostname
+var Timeout Duration
+var ScanInterval Duration
+var LogLevel slog.Level
+var LogAddSource bool
+
+type Hostname string
+type Duration time.Duration
+type Params struct {
+	DNSresolvers []net.IP   `json:"dnsResolvers"`
+	Hostnames    []Hostname `json:"hostnames"`
+	Timeout      Duration   `json:"timeout"`
+	ScanInterval Duration   `json:"scanInterval"`
+	LogLevel     slog.Level `json:"logLevel"`
+	LogAddSource bool       `json:"logAddSource"`
 }
 
-// TODO: Take parameter
-func Parse() (*config, error) {
-	file, fileErr := os.ReadFile("config.json")
-	if fileErr != nil {
-		return nil, fileErr
+func (h *Hostname) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
 	}
 
-	var config config
-	if err := json.Unmarshal(file, &config); err != nil {
-		return nil, err
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := validate.Var(s, "hostname_rfc1123"); err != nil {
+		return err
 	}
-	return &config, nil
+
+	*h = Hostname(s)
+	return nil
+}
+
+func (d *Duration) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+	dur, err := time.ParseDuration(s)
+	*d = Duration(dur)
+	return err
+}
+
+func load(p *Params) error {
+	data, err := os.ReadFile(configFilePath)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, &p)
+	return err
+}
+
+func init() {
+	configLog := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	err := load(&Current)
+	if err != nil {
+		configLog.Error(
+			"failed to load configuration parameters",
+			"error", err.Error(),
+		)
+		os.Exit(1)
+	}
+	configLog.Info(
+		"configuration parameters loaded",
+		"params", Current,
+	)
+	DNSresolvers = Current.DNSresolvers
+	Hostnames = Current.Hostnames
+	Timeout = Current.Timeout
+	ScanInterval = Current.ScanInterval
+	LogLevel = Current.LogLevel
+	LogAddSource = Current.LogAddSource
 }
