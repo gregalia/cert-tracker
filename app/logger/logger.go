06d70c86fd03f05f0026a4b3359ee993e3db7416
Greg Sheppard <gregalia@users.noreply.github.com>
2025-06-18 19:24:28 +0300

    refactor: Simplify logger and use JSON config
    Didn't know about slog.HandlerOptions.AddSource before and happy to
    trade more simplicity for less control.

--- app/logger/logger.go
+++ app/logger/logger.go
@@ -1,39 +1,12 @@
 package logger
 
 import (
-	"context"
+	"cert-tracker/cfg"
 	"log/slog"
-	"runtime"
+	"os"
 )
 
-type LocationHandler struct {
-	Handler       slog.Handler
-	LogTraceLevel slog.Level
-}
-
-func (h *LocationHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	return h.Handler.Enabled(ctx, level)
-}
-
-func (h *LocationHandler) Handle(ctx context.Context, r slog.Record) error {
-	if r.Level >= h.LogTraceLevel {
-		// Skip 3 frames: Handle + log function + user code
-		pc, file, line, ok := runtime.Caller(3)
-		if ok {
-			r.Add(
-				"file", slog.StringValue(file),
-				"line", slog.IntValue(line),
-				"function", slog.StringValue(runtime.FuncForPC(pc).Name()),
-			)
-		}
-	}
-	return h.Handler.Handle(ctx, r)
-}
-
-func (h *LocationHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return &LocationHandler{h.Handler.WithAttrs(attrs), h.LogTraceLevel}
-}
-
-func (h *LocationHandler) WithGroup(name string) slog.Handler {
-	return &LocationHandler{h.Handler.WithGroup(name), h.LogTraceLevel}
-}
+var Log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+	AddSource: cfg.LogAddSource,
+	Level: cfg.LogLevel,
+}))
